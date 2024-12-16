8b94e0f1f5f4fb3be151a20af7604f803322d143
mniu0602 <mniu0602@gmail.com>
2024-12-16 18:35:02 +0000

    Initial c]mmit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyB-9J_tiUjt0atnsErxhllq9x7u9LFLZKQ',
+      authDomain: 'sample-firebase-ai-app-fedb2.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-fedb2',
+      storageBucket: 'sample-firebase-ai-app-fedb2.firebasestorage.app',
+      messagingSenderId: '51699892673',
+      appId: '1:51699892673:web:d1a307819f182654d7af40',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyA8TZpoC9NOTQFUwb_IU9m37NYj48ZNygg',
+  debug_token: ''
+};
